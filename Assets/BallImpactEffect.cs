8b3c3a3d69723122e62e375dff92d83f25538b04
Kershockr <tim.merino@live.com>
2023-10-15 20:26:02 -0400

    added simple impact particle effect

---
+++ Assets/BallImpactEffect.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallImpactEffect : MonoBehaviour
+{
+    public ParticleSystem dustEffect;
+    public float impactForceThresh = 1;
+    public float impactMultiplier = 1;
+    public float offsetAmount = 1;
+
+    private void Start()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        float impactForce = collision.relativeVelocity.magnitude;
+        Debug.Log(impactForce);
+
+        if (impactForce > impactForceThresh) // Adjust this threshold as needed
+        {
+            // The point of impact
+            ContactPoint contact = collision.contacts[0];
+            Vector3 offsetPosition = contact.point + (contact.normal * offsetAmount);
+            dustEffect.transform.position = offsetPosition;
+
+            int burstAmount = Mathf.RoundToInt(impactForce * impactMultiplier);
+            dustEffect.Emit(burstAmount);
+            // Adjust particle emission based on impact force
+            //var emission = dustEffect.emission;
+            //emission.SetBurst(0, new ParticleSystem.Burst(0, impactForce * impactMultiplier)); // Multiplying by 10 for more particles
+
+            //var main = dustEffect.main;
+            //main.startSpeed = impactForce;
+
+            //dustEffect.Play();
+        }
+    }
+}
